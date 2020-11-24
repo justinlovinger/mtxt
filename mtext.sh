020cc5ec06b6d5f4214c26e22fd2ac8db26fe3dc
Justin Lovinger <git@justinlovinger.com>
2020-11-23 20:32:45 -0500

    Remove `--` requirement
    To compensate,
    you can't add more than one value
    to an option.

--- mtext.sh
+++ mtext.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 opts=()
+positionals=()
 
-if [[ "$1" = -* ]]; then
-  positionals=()
-
-  while [[ $# -gt 0 ]]; do
-    case "$1" in
-      --)
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --)
+      shift
+      while [[ $# -gt 0 ]]; do
+        positionals+=("$1")
         shift
-        while [[ $# -gt 0 ]]; do
-          positionals+=("$1")
-          shift
-        done
-        ;;
-      *)
-        opts+=("$1")
-        shift
-        ;;
-    esac
-  done
+      done
+      ;;
+    -*)
+      opts+=("$1" "$2")
+      shift
+      shift
+      ;;
+    *)
+      positionals+=("$1")
+      shift
+      ;;
+  esac
+done
 
-  set -- "${positionals[@]}" # restore positional parameters
-fi
+set -- "${positionals[@]}" # restore positional parameters
 
 if [[ $# -gt 0 ]]; then
   bodyf=$(mktemp)
